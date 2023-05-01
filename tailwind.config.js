3da62791f8acb2f22f43f6cc72a8607e92fb8941
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-04-30 20:52:43 -0500

    add tailwind and angular cdk styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   darkMode: 'media',
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        trello: {
+          primary: '#5AAC44',
+          secondary: '#0052cc',
+          terciary: '#0065ff'
+        },
+        success: colors.green
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
