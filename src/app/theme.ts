fdaaef9cfff22ef5880b5f15f49918e2f5d122fe
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-04-30 20:54:05 -0500

    add themes script

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,9 +1,13 @@
 const wrapperDefaultClasses = 'min-h-screen min-w-screen';
-const bodyClass: Record<string, string[]> = {
-  dark: ['bg-slate-900','text-slate-100'],
-  light: ['bg-white-900','text-slate-900']
-}
 
+const rgbBgColors: Record<string, string> = {
+  light: 'rgb(15,23,42)',
+  dark: 'rgb(241,245,249)'
+};
+const rgbTextColors: Record<string, string> = {
+  dark: 'rgb(15,23,42)',
+  light: 'rgb(241,245,249)'
+};
 function getDomElementById(id: string) {
   const element: HTMLElement | null = document.getElementById(id);
 
@@ -14,22 +18,22 @@ function getDomElementById(id: string) {
 }
 
 function getCurrentTheme(): string {
-  const body = getDomElementById('wrapper');
+  const body = getDomElementById('body');
   const theme: string = body!.getAttribute('theme')!;
   return theme;
 }
 
 function updateThemeAttr(theme: 'dark' | 'light') {
-  const body = getDomElementById('wrapper');
+  const body = getDomElementById('body');
   body!.setAttribute('theme', theme);
 }
 
 
 export function setTheme(theme: 'dark' | 'light') {
   updateThemeAttr(theme);
-  const wrapperRef = getDomElementById('wrapper');
-  wrapperRef.setAttribute('class', wrapperDefaultClasses);
-  wrapperRef.classList.add(...bodyClass[theme])
+  const body = getDomElementById('body');
+  body.style.backgroundColor = rgbBgColors[theme];
+  body.style.color = rgbTextColors[theme];
 }
 
 export function toggleTheme() {
